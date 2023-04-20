9a474ecbeaee89e0b2caba68512014fcb44ba3f5
Szabó Szebasztián <szaboszebasztianlevente@ktch.hu>
2023-04-20 13:23:58 +0200

    first commit

--- src/Kamatos.java
+++ src/Kamatos.java
@@ -7,7 +7,7 @@ public class Kamatos {
     }
 
     public void startKamatos(){
-        double tenyleges = this.bekerdata();
+        double tenyleges = this.bekerAdat();
         nyomtat(tenyleges);
 
     }
@@ -17,10 +17,33 @@ public class Kamatos {
 
     }
 
-    private double bekerdata(){
+    private double bekerAdat(){
         String betetStr = beker("Betét: ");
+        this.checkInput(betetStr);
+
+        if(!this.checkInput(betetStr)){
+            System.err.println("Hibás karakter");
+
+            throw new NumberFormatException();
+
+        }
+
         String nevlegesStr = beker("Névleges kamat (%): ");
+
+        if(!this.checkInput(nevlegesStr)){
+            System.err.println("nem");
+
+            throw new NumberFormatException();
+
+        }
         String tokesites_szamStr = beker("Évenkénti tőkésítési szám: ");
+
+        if(!this.checkInput(tokesites_szamStr)){
+            System.err.println("nem lyo");
+
+            throw new NumberFormatException();
+
+        }
 
         double betet = Double.parseDouble(betetStr);
         double nevleges = Double.parseDouble(nevlegesStr);
